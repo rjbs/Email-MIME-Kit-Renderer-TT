b4b138e4bcdf7190c4b945a836a693274e319419
Ricardo Signes <rjbs@cpan.org>
2010-09-04 15:35:01 -0400

    better means to pass args to TT

--- lib/Email/MIME/Kit/Renderer/TT.pm
+++ lib/Email/MIME/Kit/Renderer/TT.pm
@@ -3,7 +3,37 @@ use Moose;
 with 'Email::MIME::Kit::Role::Renderer';
 # ABSTRACT: render parts of your mail with Template-Toolkit
 
-use Template;
+use Template 2.1;
+
+=head1 DESCRIPTION
+
+This is a renderer plugin for L<Email::MIME::Kit>, and renders message parts
+using L<Template Toolkit 2|Template>.  When specifying a renderer in
+F<manifest.json>, you might write something like this:
+
+  { ..., "renderer": "TT" }
+
+Or, to supply options:
+
+  {
+    ...,
+    "renderer": [
+      "TT",
+      { ...params go here... }
+    ]
+  }
+
+There are only three parameters that can be supplied right now:
+
+C<strict> sets the C<STRICT> Template parameter.  It defaults to 1.
+
+C<eval_perl> sets the C<EVAL_PERL> Template parameter.  It defaults to 0.
+
+C<template_parameters> can be a hashref of any parameters to be passed to the
+Template constructor.  Setting C<STRICT> or C<EVAL_PERL> here overrides the
+C<strict> and C<eval_perl> options.
+
+=cut
 
 # XXX: _include_path or something
 # XXX: we can maybe default to the kit dir if the KitReader is Dir
@@ -20,32 +50,38 @@ sub render {
 }
 
 has eval_perl => (
-  is  => 'ro',
-  isa => 'Bool',
+  is   => 'ro',
+  isa  => 'Bool',
   default => 0,
 );
 
+has strict => (
+  is   => 'ro',
+  isa  => 'Bool',
+  default => 1,
+);
+
+has template_parameters => (
+  is  => 'ro',
+  isa => 'HashRef',
+  default => sub { {} },
+);
+
 has tt => (
+  is   => 'ro',
   isa  => 'Template',
   lazy => 1,
-  accessor => '_tt',
   init_arg => undef,
   default  => sub {
+    my ($self) = @_;
     Template->new({
       ABSOLUTE  => 0,
       RELATIVE  => 0,
-      EVAL_PERL => $_[0]->eval_perl,
+      STRICT    => $self->strict,
+      EVAL_PERL => $self->eval_perl,
+      %{ $self->template_parameters },
     });
   },
 );
-
-sub tt {
-  my $self = shift;
-  return $self->_tt || $self->_tt(Template->new({
-    ABSOLUTE => 1,
-    RELATIVE => 1,
-    INCLUDE_PATH => $self->_include_path_ref,
-  }));
-}
 
 1;
